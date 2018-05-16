41514c348bc6550bbdbd89717f59ac5d4f9dac42
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-05-16 09:24:18 -0600

    adds append functionality

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,6 +1,19 @@
+require './lib/node'
+
 class LinkedList
   attr_reader :head
   def initialize
-    head = nil
+    @head = nil
+  end
+
+  def append(current = @head, num)
+    if head.nil?
+      @head = Node.new(num)
+    elsif current.next_node.nil?
+      current.next_node = Node.new(num)
+    else
+      current = current.next_node
+      append(current, num)
+    end
   end
 end
--- test/linked_list_test.rb
+++ test/linked_list_test.rb
@@ -1,11 +1,28 @@
 require 'minitest'
 require 'minitest/autorun'
 require './lib/linked_list'
+require './lib/node'
 
 class LinkedListTest < Minitest::Test
   def test_it_has_a_head
     linked_list = LinkedList.new()
 
     assert linked_list.head.nil?
+  end
+
+  def test_it_can_add_a_first_node
+    linked_list = LinkedList.new()
+
+    linked_list.append(5)
+
+    assert_instance_of Node, linked_list.head
+  end
+
+  def test_it_can_add_a_second_node
+    linked_list = LinkedList.new()
+    linked_list.append(5)
+    linked_list.append(4)
+
+    assert_instance_of Node, linked_list.head.next_node
   end
 end
